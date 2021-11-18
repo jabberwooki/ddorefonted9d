9df0a9842d6ae5bc66171cd3a4f7fb7d0ab6c2a8
yvandouenel <y.douenel@coopernet.fr>
2021-11-18 14:40:24 +0100

    Version qui fonctionne pour l'affichage du moteur de recherche même si le fichier est chargé plusieurs fois quand on est loggé.

--- web/themes/custom/ddo/js/search.js
+++ web/themes/custom/ddo/js/search.js
@@ -7,17 +7,28 @@
 
   'use strict';
 
-  Drupal.behaviors.ddo_search = {
+  Drupal.behaviors.ddo = {
     attach: function (context, settings) {
+      /*if (typeof jQuery != 'undefined') {
+        // jQuery is loaded => print the version
+        console.log(jQuery.fn.jquery);
+      }*/
+      /*$('#edit-keys', context).once('ddo', function () {
+        // Code here will only be applied to $('#some_element')
+        // a single time.
+        console.log(`ONCE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
+      });*/
         console.log(`Dans search.js`);
         $("#custom-search-button").on("click", function(){
           console.log(`Click sur la loupe`);
-          $("#edit-keys").focus();
-          $("#search-row").toggleClass("visually-hidden-focusable");
+          /*$("#edit-keys").focus();*/
+          /*$("#search-row").toggleClass("visually-hidden-focusable");*/
           if($("#search-row").hasClass("visually-hidden-focusable")) {
-            document.activeElement.blur();
-          } else {
+            $("#search-row").removeClass("visually-hidden-focusable");
             $("#edit-keys").focus();
+          } else {
+            $("#search-row").addClass("visually-hidden-focusable");
+            document.activeElement.blur();
           }
         });
         $("#close-search-form").on("click", function(){
