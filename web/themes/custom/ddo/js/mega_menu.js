ea0c03db61ca0cf40f6a6a0011c9bb5ef0b1904d
yvandouenel <y.douenel@coopernet.fr>
2021-12-03 17:24:43 +0100

    JS : le js n'a plus à gérer le placement des menus dans les blocs.

--- web/themes/custom/ddo/js/mega_menu.js
+++ web/themes/custom/ddo/js/mega_menu.js
@@ -11,12 +11,6 @@
     attach: function (context, settings) {
       console.log(`Dans mega_menu.js`);
       $("#mega-menu").hide(0);
-      // Place l'élément du main menu concernant le ddo en deuxième colonne
-      $("#block-main-menu-in-mega ul.nav > li#li-mega-menu-5").appendTo("#col-2-mega-menu > ul");
-
-      // Place les menus bas en troisième colonne
-      $("#block-footer-menu-1-in-mega").appendTo("#col-3-mega-menu");
-      $("#block-footer-menu-2-in-mega").appendTo("#col-3-mega-menu");
 
       // Gestion du click sur le bouton burger menu
       $("#custom-mega-menu-button").on("click", function (event) {
