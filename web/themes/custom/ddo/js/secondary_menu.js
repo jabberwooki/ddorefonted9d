0c3dc9b524dbdf81af98941216ea0881edde7df3
yvandouenel <y.douenel@coopernet.fr>
2021-12-07 18:20:58 +0100

    JS modifié : les menus de gauche s'appuient maintenant sur l'id page-nav-left.

--- web/themes/custom/ddo/js/secondary_menu.js
+++ web/themes/custom/ddo/js/secondary_menu.js
@@ -11,7 +11,7 @@
     attach: function (context, settings) {
       console.log(`Dans secondary_menu.js`);
       // ajout d'un classe au niveau de la navigation pour indiquer si aucun lien n'est actif
-      $(".sub_main_menu nav.navigation").each(function() {
+      $("#page-nav-left nav.navigation").each(function() {
         if(!$("li.active",$(this)).length) {
           $(this).addClass("nav-inactive");
         }
