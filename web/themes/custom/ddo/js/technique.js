a851e86b1d7c6a471a4759fa65c5df3508b6add0
yvandouenel <y.douenel@coopernet.fr>
2022-01-26 12:52:08 +0100

    Amélioration technique.js.

--- web/themes/custom/ddo/js/technique.js
+++ web/themes/custom/ddo/js/technique.js
@@ -7,8 +7,11 @@
 (function ($, Drupal, drupalSettings) {
   Drupal.behaviors.ddo_technique = {
     attach: function (context, settings) {
+
       $(document, context).once('ddo_technique').each(function () {
         console.log("dans technique.js");
+        console.log("Suppresion des paragraphes documents !");
+        $(".paragraph--type--documents").parents(".custom-paragraph").addClass("open-close");
         $(".open-close .field--name-field-document-sets").hide();
         $(".open-close .layout--onecol .field--name-field-title").click(function() {
           $(this).toggleClass("open");
