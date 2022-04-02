28d840667656d0e783543dc9e7ec20046163d118
Yvan Douënel <y.douenel@coopernet.fr>
2022-04-02 12:54:33 +0200

    Amélioration du js qui permet d'ajouter une class pour cacher les menus de gauche qui n'ont pas de liens actifs.

--- web/themes/custom/ddo/js/secondary_menu.js
+++ web/themes/custom/ddo/js/secondary_menu.js
@@ -11,8 +11,10 @@
     attach: function (context, settings) {
       console.log(`Dans secondary_menu.js`);
       // ajout d'un classe au niveau de la navigation pour indiquer si aucun lien n'est actif
-      $("#page-nav-left nav.navigation").each(function() {
-        if(!$("li.active",$(this)).length) {
+      $("#page-nav-left nav.navigation").each(function () {
+        if (!$("a.is-active", $(this)).length) {
+          console.log("je suis dans un menu qui a des liens inactifs");
+          console.log($(this).attr('id'));
           $(this).addClass("nav-inactive");
         }
       });
